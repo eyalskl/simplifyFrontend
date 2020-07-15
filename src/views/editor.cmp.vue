31af7064c3be885592d5fb76b96a857ee43840b2
Eyal Barkai <eyalskl18@gmail.com>
2020-07-15 22:17:10 +0300

    styling

--- src/views/editor.cmp.vue
+++ src/views/editor.cmp.vue
@@ -1,16 +1,28 @@
 <template>
   <section class="editor flex">
-      <div class="element-picker"> </div>
-      <div class="workspace"> </div>
+    <div class="element-picker">
+      <button> Text </button>
+      <button> Button </button>
+      <button> Image </button>
+      <button> Map </button>
+      <button> Map </button>
+      <button> Map </button>
+      <button> Map </button>
+      <button> Map </button>
+      <button> Map </button>
+      <button> Map </button>
+      <button> Map </button>
+      <button> Map </button>
+    </div>
+    <div class="workspace"></div>
   </section>
 </template>
 
 <script>
 export default {
-    name: 'editor'
-}
+  name: 'editor'
+};
 </script>
 
 <style>
-
 </style>
