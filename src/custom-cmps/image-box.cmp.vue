72844dabb7d06628919a761edd571f7cb5064392
Eyal Barkai <eyalskl18@gmail.com>
2020-07-16 12:58:02 +0300

    kaki

---
+++ src/custom-cmps/image-box.cmp.vue
@@ -0,0 +1,15 @@
+<template>
+    <span>
+        <component :is="cmpTree.tagName" :src="cmpTree.attr.src" :style="cmpTree.attr.style" />
+    </span>
+</template>
+
+<script>
+export default {
+    name:"image-box",
+    props:['cmpTree'],
+    created() {
+        console.log(this.cmpTree);
+    }
+}
+</script>
