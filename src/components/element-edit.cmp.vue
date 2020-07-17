3783408c0649513f4a527119b032aabbff95c2a3
Eyal Barkai <eyalskl18@gmail.com>
2020-07-17 16:58:55 +0300

    added some kaki services

--- src/components/element-edit.cmp.vue
+++ src/components/element-edit.cmp.vue
@@ -1,43 +1,13 @@
 <template>
   <section class="element-edit">
     <!-- <input placeholder="Element's name..." v-model="cmpToEdit.cmpName" /> -->
-    <div class="flex space-between align-center">
-      <select-box :data="fonts" placeholder="Choose your font..."></select-box>
-      <el-color-picker show-alpha v-model="cmpToEdit.color"></el-color-picker>
-    </div>
-    <div class="flex space-between align-center">
-      <label>Font size:</label>
-      <el-slider v-model="cmpToEdit.fontSize"></el-slider>
-    </div>
-    <div class="align-controls flex">
-      <button> <i class="fas fa-align-left"></i> </button>
-      <button> <i class="fas fa-align-center"></i> </button>
-      <button> <i class="fas fa-align-right"></i> </button>
-    </div>
-    <div class="font-style-controls flex">
-      <button> <i class="fas fa-bold"></i> </button>
-      <button> <i class="fas fa-italic"></i> </button>
-      <button> <i class="fas fa-underline"></i> </button>
-    </div>
-    <div class="text-shadow">
-      <select-box :data="shadows" placeholder="Add text shadow..."></select-box>
-    </div>
-    <button class="advanced" @click="openAdvanced"> Advanced Settings </button>
-    <div v-if="showAdvanced" class="advanced-settings animate__animated animate__fadeInUpBig">
-      <div class="flex space-between align-center">
-      <label>Line height:</label>
-      <el-slider v-model="cmpToEdit.lineHeight"></el-slider>
-    </div>
-      <div class="flex space-between align-center">
-      <label>Letter spacing:</label>
-      <el-slider v-model="cmpToEdit.letterSpacing"></el-slider>
-    </div>
-    </div>
+    <site-text-edit />
   </section>
 </template>
 
 <script>
 import selectBox from '../custom-cmps/select-box.cmp';
+import siteTextEdit from '../custom-cmps/site-text-edit.cmp';
 
 export default {
   name: 'element-edit',
@@ -61,7 +31,8 @@ export default {
     }
   },
   components: {
-    selectBox
+    selectBox,
+    siteTextEdit
   }
 };
 </script>
