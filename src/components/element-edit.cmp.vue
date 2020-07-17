121af94798b2200b4fb59e57c9b28d817cafb2f6
Eyal Barkai <eyalskl18@gmail.com>
2020-07-17 15:44:50 +0300

    qwdad

--- src/components/element-edit.cmp.vue
+++ src/components/element-edit.cmp.vue
@@ -1,13 +1,37 @@
 <template>
   <section class="element-edit">
-    <!-- <input placeholder="Element's name..." v-model="cmpName" /> -->
+    <!-- <input placeholder="Element's name..." v-model="cmpToEdit.cmpName" /> -->
     <div class="flex space-between align-center">
-      <select-box :data="fonts"></select-box>
-      <el-color-picker show-alpha v-model="color"></el-color-picker>
+      <select-box :data="fonts" placeholder="Choose your font..."></select-box>
+      <el-color-picker show-alpha v-model="cmpToEdit.color"></el-color-picker>
     </div>
     <div class="flex space-between align-center">
       <label>Font size:</label>
-      <el-slider v-model="fontSize"></el-slider>
+      <el-slider v-model="cmpToEdit.fontSize"></el-slider>
+    </div>
+    <div class="align-controls flex">
+      <button> <i class="fas fa-align-left"></i> </button>
+      <button> <i class="fas fa-align-center"></i> </button>
+      <button> <i class="fas fa-align-right"></i> </button>
+    </div>
+    <div class="font-style-controls flex">
+      <button> <i class="fas fa-bold"></i> </button>
+      <button> <i class="fas fa-italic"></i> </button>
+      <button> <i class="fas fa-underline"></i> </button>
+    </div>
+    <div class="text-shadow">
+      <select-box :data="shadows" placeholder="Add text shadow..."></select-box>
+    </div>
+    <button class="advanced" @click="openAdvanced"> Advanced Settings </button>
+    <div v-if="showAdvanced" class="advanced-settings animate__animated animate__fadeInUpBig">
+      <div class="flex space-between align-center">
+      <label>Line height:</label>
+      <el-slider v-model="cmpToEdit.lineHeight"></el-slider>
+    </div>
+      <div class="flex space-between align-center">
+      <label>Letter spacing:</label>
+      <el-slider v-model="cmpToEdit.letterSpacing"></el-slider>
+    </div>
     </div>
   </section>
 </template>
@@ -19,11 +43,22 @@ export default {
   name: 'element-edit',
   data() {
     return {
-      cmpName: '',
-      fontSize: 20,
+      cmpToEdit: {
+        cmpName: '',
+        fontSize: 20,
+        lineHeight: 1,
+        letterSpacing: 0,
+        color: '#765fe5'
+      },
+      showAdvanced: false,
       fonts: ['Arial', 'Righteous', 'Advent Pro', 'Sans Serif', 'Tahoma'],
-      color: '#765fe5'
+      shadows: ['Light', 'Medium', 'Heavy'],
     };
+  },
+  methods: {
+    openAdvanced() {
+      this.showAdvanced = !this.showAdvanced
+    }
   },
   components: {
     selectBox
