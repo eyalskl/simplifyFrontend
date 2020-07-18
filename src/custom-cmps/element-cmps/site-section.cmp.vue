34bdbb679c55e5316e23a1cf585bda0dc8e32566
Idov <magalidov@gmail.com>
2020-07-18 16:42:39 +0300

    fh

--- src/custom-cmps/element-cmps/site-section.cmp.vue
+++ src/custom-cmps/element-cmps/site-section.cmp.vue
@@ -33,18 +33,10 @@ export default {
   },
   components: {
     siteDiv,
-<<<<<<< HEAD:src/custom-cmps/site-section.cmp.vue
     siteText,
     siteImage,
     siteButton,
     elementControls,
-=======
-    elementControls,
-    siteText,
-    siteImage,
-    siteButton,
-    siteList,
->>>>>>> c8e6465e84a1f27899d82dce5c1f2f21022f2987:src/custom-cmps/element-cmps/site-section.cmp.vue
   }
 };
 </script>
