21c29bda17c8b986d0115ef5bcb64dd1f99fbe8a
Idov <magalidov@gmail.com>
2020-07-19 11:01:10 +0300

    xcbb

--- src/views/templates-page.cmp.vue
+++ src/views/templates-page.cmp.vue
@@ -1,14 +1,35 @@
 <template>
   <section class="templates-page">
-      <h1>Choose your Template</h1>
-      <section class="template-list"> 
-        
-      </section>
+    <h1>Choose your Template</h1>
+    <section class="template-list">
+      <div
+        v-for="template in templates"
+        :key="template._id"
+        @click="editTemplate(template._id)"
+      >
+      <img :src="template.previewImg"/>
+      </div>
+    </section>
   </section>
 </template>
 
 <script>
-export default {
+import { templateService } from '@/services/template-service.js';
 
-}
+export default {
+  name: 'templates-page',
+  data() {
+    return {
+      templates: []
+    };
+  },
+  created() {
+    this.templates = templateService.getTemplates();
+  },
+  methods: {
+    editTemplate(id){
+      this.$router.push(`/editor/${id}`)
+    }
+  }
+};
 </script>
